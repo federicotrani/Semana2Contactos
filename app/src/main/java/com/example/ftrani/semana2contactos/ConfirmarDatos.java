4fd41dd23eadaf42d00b7c3f16ce9aa000410484
federicotrani <federicotrani@gmail.com>
2016-12-21 19:01:51 -0300

    Se incluye datepicker para fecha de nacimiento en pantalla de edicion

--- app/src/main/java/com/example/ftrani/semana2contactos/ConfirmarDatos.java
+++ app/src/main/java/com/example/ftrani/semana2contactos/ConfirmarDatos.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class ConfirmarDatos extends AppCompatActivity {
@@ -14,6 +13,8 @@ public class ConfirmarDatos extends AppCompatActivity {
     TextView tvEmail;
     TextView tvDescripcion;
     TextView tvFecha;
+    int dia, mes, anio;
+    String nombre, email, telefono, descripcion;
 
 
     @Override
@@ -23,11 +24,14 @@ public class ConfirmarDatos extends AppCompatActivity {
 
         Bundle p = getIntent().getExtras();
 
-        String nombre = p.getString(getResources().getString(R.string.pnombre));
-        String telefono = p.getString(getResources().getString(R.string.ptelefono));
-        String email = p.getString(getResources().getString(R.string.pemail));
-        String descripcion = p.getString(getResources().getString(R.string.pdescripcion));
-        String fecha = p.getString(getResources().getString(R.string.pfecha));
+        nombre = p.getString(getResources().getString(R.string.pnombre));
+        telefono = p.getString(getResources().getString(R.string.ptelefono));
+        email = p.getString(getResources().getString(R.string.pemail));
+        descripcion = p.getString(getResources().getString(R.string.pdescripcion));
+        //String fecha = p.getString(getResources().getString(R.string.pfecha));
+        dia = p.getInt("Dia");
+        mes = p.getInt("Mes");
+        anio = p.getInt("Anio");
 
         tvNombre = (TextView) findViewById(R.id.tvNombre);
         tvDescripcion = (TextView) findViewById(R.id.tvDescripcion);
@@ -36,10 +40,10 @@ public class ConfirmarDatos extends AppCompatActivity {
         tvFecha = (TextView) findViewById(R.id.tvFecha);
 
         tvNombre.setText(nombre);
-        tvDescripcion.setText("Descripción: "+descripcion);
-        tvTelefono.setText("Tel: "+telefono);
-        tvEmail.setText("Email: "+email);
-        tvFecha.setText("Fecha Nacimiento: "+fecha);
+        tvDescripcion.setText(String.format("Descripción: %s", descripcion));
+        tvTelefono.setText(String.format("Tel: %s", telefono));
+        tvEmail.setText(String.format("Email: %s",email));
+        tvFecha.setText(String.format("Fecha Nacimiento: %1d de %2s de %3d",dia,mostrarMes(mes),anio));
 
     }
 
@@ -48,13 +52,61 @@ public class ConfirmarDatos extends AppCompatActivity {
         //objeto que vincula las activity origen y destino
         Intent intent = new Intent(this, MainActivity.class);
 
-        intent.putExtra(getResources().getString(R.string.pnombre), tvNombre.getText().toString());
-        intent.putExtra(getResources().getString(R.string.ptelefono), tvTelefono.getText().toString());
-        intent.putExtra(getResources().getString(R.string.pemail), tvEmail.getText().toString());
-        intent.putExtra(getResources().getString(R.string.pdescripcion), tvDescripcion.getText().toString());
-        intent.putExtra(getResources().getString(R.string.pfecha), tvFecha.getText().toString());
+        intent.putExtra(getResources().getString(R.string.pnombre), this.nombre);
+        intent.putExtra(getResources().getString(R.string.ptelefono), this.telefono);
+        intent.putExtra(getResources().getString(R.string.pemail), this.email);
+        intent.putExtra(getResources().getString(R.string.pdescripcion), this.descripcion);
+        intent.putExtra(getResources().getString(R.string.pdia), this.dia);
+        intent.putExtra(getResources().getString(R.string.pmes), this.mes);
+        intent.putExtra(getResources().getString(R.string.panio), this.anio);
 
         startActivity(intent);
         finish();
+    }
+
+    protected String mostrarMes(int mes){
+
+        String valor = "";
+        switch(mes){
+            case 1:
+                valor = "Enero";
+                break;
+            case 2:
+                valor = "Febrero";
+                break;
+            case 3:
+                valor = "Marzo";
+                break;
+            case 4:
+                valor = "Abril";
+                break;
+            case 5:
+                valor = "Mayo";
+                break;
+            case 6:
+                valor = "Junio";
+                break;
+            case 7:
+                valor = "Julio";
+                break;
+            case 8:
+                valor = "Agosto";
+                break;
+            case 9:
+                valor = "Setiembre";
+                break;
+            case 10:
+                valor = "Octubre";
+                break;
+            case 11:
+                valor = "Noviembre";
+                break;
+            case 12:
+                valor = "Diciembre";
+                break;
+        }
+
+        return valor;
+
     }
 }
--- app/src/main/java/com/example/ftrani/semana2contactos/MainActivity.java
+++ app/src/main/java/com/example/ftrani/semana2contactos/MainActivity.java
@@ -6,25 +6,28 @@ import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText edtNombre;
-    private EditText edtFecha;
+    private DatePicker dtpFecha;
     private EditText edtTelefono;
     private EditText edtEmail;
     private EditText edtDescripcion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         edtNombre = (EditText) findViewById(R.id.edtNombre);
-        edtFecha = (EditText) findViewById(R.id.edtFecha);
+        //edtFecha = (EditText) findViewById(R.id.edtFecha);
         edtTelefono = (EditText) findViewById(R.id.edtTelefono);
         edtEmail = (EditText) findViewById(R.id.edtEmail);
         edtDescripcion = (EditText) findViewById(R.id.edtDescripcion);
+        dtpFecha = (DatePicker) findViewById(R.id.dtpFecha);
 
         //capturo parametros de activity previa
         if(getIntent().getExtras()!=null){
@@ -35,14 +38,21 @@ public class MainActivity extends AppCompatActivity {
             String telefono = p.getString(getResources().getString(R.string.ptelefono));
             String email = p.getString(getResources().getString(R.string.pemail));
             String descripcion = p.getString(getResources().getString(R.string.pdescripcion));
-            String fecha = p.getString(getResources().getString(R.string.pfecha));
+            //String fecha = p.getString(getResources().getString(R.string.pfecha));
+            int dia = p.getInt(getResources().getString(R.string.pdia));
+            int mes = p.getInt(getResources().getString(R.string.pmes));
+            int anio = p.getInt(getResources().getString(R.string.panio));
+
 
             //asigno parametros a controles de edición
             edtNombre.setText(nombre);
-            edtFecha.setText(fecha);
+            dtpFecha.updateDate(dia,mes,anio);
+            //edtFecha.setText(fecha);
             edtTelefono.setText(telefono);
             edtEmail.setText(email);
             edtDescripcion.setText(descripcion);
+
+
         }
 
     }
@@ -53,11 +63,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(this, ConfirmarDatos.class);
 
                 //intent.putExtra(getResources().getString(R.string.pnombre), edtNombre.getText());
-                intent.putExtra(getResources().getString(R.string.pnombre), edtNombre.getText().toString());
+                intent.putExtra(getResources().getString(R.string.pnombre), edtNombre.getText().toString().toUpperCase());
                 intent.putExtra(getResources().getString(R.string.ptelefono),edtTelefono.getText().toString());
                 intent.putExtra(getResources().getString(R.string.pemail),edtEmail.getText().toString());
                 intent.putExtra(getResources().getString(R.string.pdescripcion),edtDescripcion.getText().toString());
-                intent.putExtra(getResources().getString(R.string.pfecha),edtFecha.getText().toString());
+                //intent.putExtra(getResources().getString(R.string.pfecha),edtFecha.getText().toString());
+                intent.putExtra(getResources().getString(R.string.pdia), dtpFecha.getDayOfMonth());
+                intent.putExtra(getResources().getString(R.string.pmes), dtpFecha.getMonth());
+                intent.putExtra(getResources().getString(R.string.panio), dtpFecha.getYear());
 
                 startActivity(intent);
                 finish();
